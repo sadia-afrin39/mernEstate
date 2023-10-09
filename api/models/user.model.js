aaae1d505cb3b062a91f057b9998e3328a851bc5
Sadia Afrin Tarin <tarin.cse5.bu@gmail.com>
2023-10-09 10:21:01 +0600

    created user model

---
+++ api/models/user.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        require: true,
+        unique: true,
+    },
+    email:{
+        type: String,
+        require: true,
+        unique: true,
+    },
+    password:{
+        type: String,
+        require: true,
+    }
+}, {timestamps: true}); //add insertion and updation time
+
+const User = mongoose.model('User',userSchema);
+export default User;
