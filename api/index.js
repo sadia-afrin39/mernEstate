118c72ff257e2f8a6461ed9a00a9a60fbbd38fa2
Sadia Afrin Tarin <tarin.cse5.bu@gmail.com>
2023-10-10 01:19:22 +0600

    signup api

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
 });
 
 const app = express();
+app.use(express.json());
 
 app.listen(5000, ()=> {
     console.log('Server is running on port 5000');
@@ -19,3 +21,5 @@ app.listen(5000, ()=> {
 
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
