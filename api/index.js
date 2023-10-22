d36fc90309f48d6ce5df5efe317a5e12c2362540
Sadia Afrin Tarin <tarin.cse5.bu@gmail.com>
2023-10-22 20:48:49 +0600

    Signup page Backend

--- api/index.js
+++ api/index.js
@@ -1,24 +1,25 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors'; 
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 
-dotenv.config();
-
-mongoose.connect(process.env.MONGO).then(()=>{
-      console.log("conneted to MongoDB");
-}).catch((err)=>{
-    console.log(err);
-});
-
 const app = express();
+
 app.use(express.json());
+app.use(cors());
+dotenv.config();
 
 app.listen(5000, ()=> {
     console.log('Server is running on port 5000');
 });
 
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("conneted to MongoDB");
+}).catch((err)=>{
+  console.log(err);
+});
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
