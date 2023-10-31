c593a336470704d20580b25bd8caedf0c19d4835
Sadia Afrin Tarin <tarin.cse5.bu@gmail.com>
2023-11-01 00:46:22 +0600

    Signin with google

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -14,7 +14,11 @@ const userSchema = new mongoose.Schema({
     password:{
         type: String,
         require: true,
-    }
+    },
+    avatar:{
+        type: String,
+        default: "https://static.vecteezy.com/system/resources/thumbnails/009/734/564/small/default-avatar-profile-icon-of-social-media-user-vector.jpg",
+    },
 }, {timestamps: true}); //add insertion and updation time
 
 const User = mongoose.model('User',userSchema);
