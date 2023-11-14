1c6389d97cfc2035b1142cab637cec29f5ae9af5
Sadia Afrin Tarin <tarin.cse5.bu@gmail.com>
2023-11-15 00:30:23 +0600

    Listing backend

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 
 const app = express();
@@ -23,6 +24,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
 
 //Middleware
 app.use((err, req, res, next) => {
