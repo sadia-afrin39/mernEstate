4cc34479db865f149c4d788e8f83f4cf4a332021
Sadia Afrin Tarin <tarin.cse5.bu@gmail.com>
2023-11-26 23:53:11 +0600

    Update listing frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Profile from './pages/Profile';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
+import UpdateListing from './pages/UpdateListing';
 
 export default function App() {
   return (
@@ -17,9 +18,11 @@ export default function App() {
         <Route path='/signin' element={<SignIn />} />
         <Route path='/signup' element={<SignUp />} />
         <Route path='/about' element={<About />} />
+
         <Route element={<PrivateRoute />} >
           <Route path='/profile' element={<Profile />} />
           <Route path='/createlisting' element={<CreateListing />} />
+          <Route path='/updatelisting/:listingId' element={<UpdateListing />} />
         </Route>  
       </Routes>
     </BrowserRouter>
