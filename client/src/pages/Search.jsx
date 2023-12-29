bcc753149caadb7233da985fb66dd163472bcb33
sadia-afrin39 <tarin.cse5.bu@gmail.com>
2023-12-30 00:56:00 +0600

    bugs fixed

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -116,7 +116,7 @@ export default function Search() {
   const onShowMoreClick = async () => {
     const numberOfListings = listings.length;
     const startIndex = numberOfListings;
-    const urlParams = new URLSearchParams(location.search);
+    const urlParams = new URLSearchParams(window.location.search);
     urlParams.set('startIndex', startIndex);
     const searchQuery = urlParams.toString();
     const res = await fetch(`/api/listing/get?${searchQuery}`);
