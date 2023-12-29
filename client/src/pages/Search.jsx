0917d4d5e48e826ce59968dfbe972b668a0ed20e
sadia-afrin39 <tarin.cse5.bu@gmail.com>
2023-12-29 22:18:17 +0600

    show more listing functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -112,6 +112,21 @@ export default function Search() {
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
   };
+
+  const onShowMoreClick = async () => {
+    const numberOfListings = listings.length;
+    const startIndex = numberOfListings;
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('startIndex', startIndex);
+    const searchQuery = urlParams.toString();
+    const res = await fetch(`/api/listing/get?${searchQuery}`);
+    const data = await res.json();
+    if (data.length < 9) {
+      setShowMore(false);
+    }
+    setListings([...listings, ...data]);
+  };
+  
   return (
     <div className='flex flex-col md:flex-row'>
         <div className='p-7 border-b-2 md:border-r-2 md:min-h-screen'>
@@ -229,6 +244,15 @@ export default function Search() {
               listings.map((listing) => (
                 <ListingItem key={listing._id} listing={listing} />
               ))}
+
+            {showMore && (
+            <button
+              onClick={onShowMoreClick}
+              className='text-green-700 hover:underline p-7 text-center w-full'
+            >
+              Show more
+            </button>
+            )}  
           </div>
         </div>
     </div>
