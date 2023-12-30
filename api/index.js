12b7f4cef02b91d9158333b27e1628a1e151915f
sadia-afrin39 <tarin.cse5.bu@gmail.com>
2023-12-30 21:52:35 +0600

    bug fix

--- api/index.js
+++ api/index.js
@@ -32,7 +32,7 @@ app.use('/api/listing', listingRouter);
 app.use(express.static(path.join(__dirname, '/client/build')));  //run the client side on server
 
 app.get('*',(req,res) => {
-    res.sendFile(path.join(__dirname, 'client','build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client','build','public','index.html'));
 })
 
 //Middleware
